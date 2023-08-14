c2d10af92251f9770f4bd1b9fb0cb39b5bf2b2c0
Scott Davis <scott@thirstyhead.com>
2023-08-14 15:18:40 -0600

    can now display wcag errors OR lh errors per page

--- docs/components/error-table/index.js
+++ docs/components/error-table/index.js
@@ -8,6 +8,7 @@ class ErrorTable extends HTMLElement{
     }
 
     get bodyBlock(){
+      let errorType = this.dataset.errorType;
       let bodyBlock = document.createElement('div');
       let details = document.createElement('details');
 
@@ -21,8 +22,9 @@ class ErrorTable extends HTMLElement{
       let thCount = document.createElement("th");
       thCount.appendChild(document.createTextNode("Count"));
       trHeader.appendChild(thCount);
-      let thCorresponds = document.createElement("th");
-      if(this.dataset.errorType == 'lighthouse'){
+      // either 'wcag' or 'lighthouse' depending on data-error-type
+      let thCorresponds = document.createElement("th");      
+      if(errorType == 'lighthouse'){
         thCorresponds.appendChild(document.createTextNode("WCAG"));
       }else{
         thCorresponds.appendChild(document.createTextNode("Lighthouse"));
@@ -31,30 +33,57 @@ class ErrorTable extends HTMLElement{
       trHeader.appendChild(thCorresponds);
 
       // build body
-      let errorCount = 0;
-      let errorType = this.dataset.errorType;
+      let lhErrorCount = 0;
+      let wcagErrorCount = 0;
       let errors = JSON.parse(sessionStorage.getItem(`jsonA11yErrors`));
       let lh2wcag = JSON.parse(sessionStorage.getItem(`lh2wcag`));
 
-      errors.forEach(function(err) {
-        let tr = table.insertRow();
-        let error = tr.insertCell();
-        error.appendChild(document.createTextNode(err.id));
-        let count = tr.insertCell();
-        count.appendChild(document.createTextNode(err.errorCount));
-        let rules = tr.insertCell();
-        rules.appendChild(document.createTextNode(lh2wcag[err.id] || "N/A"));
+      errors.forEach((err) => {
+        // either 'wcag' or 'lighthouse' depending on data-error-type
+        if(errorType == 'lighthouse'){
+            let tr = table.insertRow();
+            let error = tr.insertCell();
+            error.appendChild(document.createTextNode(err.id));
+            let count = tr.insertCell();
+            count.appendChild(document.createTextNode(err.errorCount));
+            let rules = tr.insertCell();
+            rules.appendChild(document.createTextNode(lh2wcag[err.id] || "N/A"));
 
-        errorCount += err.errorCount;
-      })
+            lhErrorCount += err.errorCount;
+        }else{
+            let wcags = lh2wcag[err.id] || [];
+            wcags.forEach((wcag) => {
+                let tr = table.insertRow();
+                let error = tr.insertCell();
+                error.appendChild(document.createTextNode(wcag));
+                let count = tr.insertCell();
+                count.appendChild(document.createTextNode(err.errorCount));
+                let rules = tr.insertCell();
+                rules.appendChild(document.createTextNode(err.id));
+
+                wcagErrorCount += err.errorCount;
+            });
+        }    
+      });
 
       let summary = document.createElement('summary');
-      summary.appendChild(document.createTextNode(`Errors: ${errorCount}`));
-      if(errorCount > 0){
-        summary.classList.add('fail');
+      if(errorType == 'lighthouse'){
+        summary.appendChild(document.createTextNode(`Errors: ${lhErrorCount}`));
+        if(lhErrorCount > 0){
+            summary.classList.add('fail');
+        }else{
+            summary.classList.add('pass');
+        }    
       }else{
-        summary.classList.add('pass');
+        summary.appendChild(document.createTextNode(`Errors: ${wcagErrorCount}`));
+        if(wcagErrorCount > 0){
+            summary.classList.add('fail');
+        }else{
+            summary.classList.add('pass');
+        }
+
       }
+      
       details.appendChild(summary);
       details.appendChild(table);
 
