65c7a8d294dd95dbb89dc95e0121bded0969e4f8
Scott Davis <scott@thirstyhead.com>
2023-08-15 15:49:27 -0600

    working on async error requests

--- docs/components/error-table-wcag/index.js
+++ docs/components/error-table-wcag/index.js
@@ -26,12 +26,16 @@ class ErrorTableWcag extends HTMLElement{
     thCorresponds.appendChild(document.createTextNode("Lighthouse"));     
     trHeader.appendChild(thCorresponds);
 
+    // fetch data
+    let errors = this.fetchJson()
+      .then((data) => { console.log('xxx' + data); });
+    let lh2wcag = JSON.parse(sessionStorage.getItem(`lh2wcag`));
+    
     // build body
     let errorCount = 0;
-    let errors = JSON.parse(sessionStorage.getItem(`jsonA11yErrors`));
-    let lh2wcag = JSON.parse(sessionStorage.getItem(`lh2wcag`));
-
-    errors.forEach((err) => {
+    // errors.forEach((err) => {
+    for(let i=0; i < errors.length; i++){  
+      let err = errors[i];
       let wcags = lh2wcag[err.id] || [];
       wcags.forEach((wcag) => {
         let tr = table.insertRow();
@@ -44,7 +48,7 @@ class ErrorTableWcag extends HTMLElement{
 
         errorCount += err.errorCount;
       });
-    });
+    }
 
     // build summary
     let summary = document.createElement('summary');
@@ -60,6 +64,30 @@ class ErrorTableWcag extends HTMLElement{
     bodyBlock.appendChild(details); 
     return bodyBlock;
   }
+
+  /**
+   * Fetches json from data website
+   */ 
+  async fetchJson(){
+    // build URL
+    const querystring = new URLSearchParams(document.location.search);
+    let website = querystring.get("website");
+    let baseUrl = `https://section508pro.github.io/508-${website}-data`;
+    // need type, date
+    let type = 'mobile';
+    let date = '2023-08-08';
+    let dateUrl = `${baseUrl}/reports/${type}/${date}`;
+    //need webpage
+    let webpage = '/';
+    let pageUrl = `${dateUrl}/reports${webpage}`;
+    let url = `${pageUrl}/a11y-errors.json`;
+
+    await fetch(url)
+      .then((response) => { return response.json(); })
+      .then((json) => { return json; })
+      .catch((e) => {console.error(e); });
+  }
+
 }  
   
 window.customElements.define('error-table-wcag', ErrorTableWcag);
