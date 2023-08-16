e4904fd661e1bb112293b61e82904f03497ad79f
Scott Davis <scott@thirstyhead.com>
2023-08-16 15:53:43 -0600

    added pageSelector

--- docs/components/report-selectors/index.js
+++ docs/components/report-selectors/index.js
@@ -16,16 +16,19 @@ class ReportSelectors extends HTMLElement{
         <input type="hidden" value=${website} name="website">
 
         <fieldset>
-          <label for="typeSelector">Type</label>
+          <label for="typeSelector">Type: </label>
           <select id="typeSelector" name="type">
             <option value="mobile">Mobile</option>
             <option value="desktop">Desktop</option>
           </select>
 
-          <label for="dateSelector">Date</label>
+          <label for="dateSelector">Date: </label>
           <select id="dateSelector" name="date">
-            <option value="2023-08-08">2023-08-08</option>
-            <option value="2023-08-15">2023-08-15</option>
+          </select>
+
+          <br>
+          <label for="pageSelector">Page: </label>
+          <select id="pageSelector" name="page">
           </select>
 
           <input type="submit" value="Update">
