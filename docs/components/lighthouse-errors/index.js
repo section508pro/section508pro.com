17ef749794474b8eb4178d98aad2c691f4035022
Scott Davis <scott@thirstyhead.com>
2023-08-16 11:58:37 -0600

    lighthouse-errors now dynamically updates

---
+++ docs/components/lighthouse-errors/index.js
@@ -0,0 +1,51 @@
+class LighthouseErrors extends HTMLElement{
+  constructor(){
+    super();
+  }
+
+  connectedCallback(){      
+    this.appendChild(this.bodyBlock);
+  }
+
+  get bodyBlock(){
+    let bodyBlock = document.createElement('div');
+    let details = document.createElement('details');
+    let table = document.createElement('table');
+    table.setAttribute('id', 'lighthouseErrorsTable');
+    
+    // build header
+    let tHead = document.createElement("thead");
+    let trHeader = tHead.insertRow();
+
+    let thError = document.createElement("th");
+    thError.appendChild(document.createTextNode("Error"));
+    trHeader.appendChild(thError);
+
+    let thCount = document.createElement("th");
+    thCount.appendChild(document.createTextNode("Count"));
+    trHeader.appendChild(thCount);
+
+    let thCorresponds = document.createElement("th");      
+    thCorresponds.appendChild(document.createTextNode("WCAG"));     
+    trHeader.appendChild(thCorresponds);
+
+    tHead.appendChild(trHeader);
+    table.appendChild(tHead);
+   
+    // build body (stubbed out; no data)
+    let tBody = document.createElement("tbody");
+    table.appendChild(tBody);
+    details.appendChild(table);
+
+    // build summary
+    let summary = document.createElement('summary');
+    summary.setAttribute('id', 'lighthouseErrorsSummary');
+    summary.appendChild(document.createTextNode(`Errors:`));
+    details.appendChild(summary);    
+
+    bodyBlock.appendChild(details); 
+    return bodyBlock;
+  }
+}  
+  
+window.customElements.define('lighthouse-errors', LighthouseErrors);
