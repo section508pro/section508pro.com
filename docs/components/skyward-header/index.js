30d06901974d08e41d809ee5d20017b3cebfb63d
Scott Davis <scott@thirstyhead.com>
2023-08-24 09:48:16 -0600

    added skyward-header

---
+++ docs/components/skyward-header/index.js
@@ -0,0 +1,63 @@
+class SkywardHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.appendChild(this.bodyBlock);
+    }
+
+    get bodyBlock() {
+        let bodyBlock = document.createElement('div');
+        bodyBlock.innerHTML = `
+        <a class="usa-skipnav" href="#main-content">Skip to main content</a>
+
+        <!-- usa-header -->
+        <div class="usa-overlay"></div>
+        <header class="usa-header usa-header--basic">
+            <div class="usa-nav-container">
+                <div class="usa-navbar">
+                    <div class="usa-logo" id="-logo">
+                        <em class="usa-logo__text"><a href="/" title="Section 508 Pro">Section 508 Pro</a></em>
+                    </div>
+                    <button type="button" class="usa-menu-btn">Menu</button>
+                </div>
+                <nav aria-label="Primary navigation," class="usa-nav">
+                    <button type="button" class="usa-nav__close">
+                        <img src="/assets/uswds/img/usa-icons/close.svg" role="img" alt="Close" />
+                    </button>
+                    <ul class="usa-nav__primary usa-accordion">
+                        <li class="usa-nav__primary-item">
+                            <a href="/public.html" class="usa-nav-link"><span>Public Webpages</span></a>
+                        </li>
+                        <li class="usa-nav__primary-item">
+                            <a href="/internal.html" class="usa-nav-link"><span>Internal Webpages</span></a>
+                        </li>
+                        <li class="usa-nav__primary-item">
+                            <a href="/documents.html" class="usa-nav-link"><span>Public Documents</span></a>
+                        </li>
+                        <li class="usa-nav__primary-item">
+                            <a href="/assessment.html" class="usa-nav-link"><span>Full Assessment</span></a>
+                        </li>
+                    </ul>
+                    <section aria-label="Search component">
+                        <form class="usa-search usa-search--small" role="search">
+                            <label class="usa-sr-only" for="search-field">Search</label>
+                            <input class="usa-input" id="search-field" type="search" name="search" />
+                            <button class="usa-button" type="submit">
+                                <img src="/assets/uswds/img/usa-icons-bg/search--white.svg" class="usa-search__submit-icon"
+                                    alt="Search" />
+                            </button>
+                        </form>
+                    </section>
+                </nav>
+            </div>
+        </header>
+        <!-- usa-header -->
+      `;
+
+        return bodyBlock;
+    }
+}
+
+window.customElements.define('skyward-header', SkywardHeader);
